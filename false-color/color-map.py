c805e2e581786664ea32c8aa6626144f952362ba
Nguyễn Gia Phong <mcsinyx@disroot.org>
2020-07-06 16:45:08 +0700

    Implement color mapping

--- false-color/color-map.py
+++ false-color/color-map.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+from csv import reader
+
 from cv2 import (COLORMAP_TURBO, IMREAD_GRAYSCALE,
                  IMWRITE_PNG_COMPRESSION, applyColorMap as colormap,
                  imread, imshow, imwrite, waitKey as wait_key)
-from numpy import uint8
+from numpy import stack, uint8, vectorize
+
+PNG_COMPRESSION = IMWRITE_PNG_COMPRESSION, 9
+with open('turbo.csv') as turbo: TURBO = list(reader(turbo))
 
 
 def disp(image, name):
@@ -11,9 +16,19 @@ def disp(image, name):
     wait_key()
 
 
+def map_color(grey, mapping):
+    """Return a color image from the given grey image and mapping."""
+    r = vectorize(lambda i: mapping[i][0])
+    g = vectorize(lambda i: mapping[i][1])
+    b = vectorize(lambda i: mapping[i][2])
+    # OpenCV uses BGR by default for whatever reason.
+    return stack((b(grey), g(grey), r(grey)), axis=-1)
+
+
 heightmapper = imread('heightmapper.png', IMREAD_GRAYSCALE)
 heightmapper_turbo = colormap(heightmapper, COLORMAP_TURBO)
 disp(heightmapper, 'original')
-disp(heightmapper_turbo, 'colormapped')
-imwrite('heightmapper-turbo.png', heightmapper_turbo,
-        (IMWRITE_PNG_COMPRESSION, 9))
+disp(heightmapper_turbo, "OpenCV's turbo")
+disp(map_color(heightmapper, TURBO), 'manually mapped turbo')
+imwrite('heightmapper-opencv.png', heightmapper_turbo, PNG_COMPRESSION)
+imwrite('heightmapper-manual.png', heightmapper_turbo, PNG_COMPRESSION)
