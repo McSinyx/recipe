6fe0edeb7a9c4a1f6ed4278af5b9bb9228fb0ba2
Nguyễn Văn Tùng <tungnv@eway.vn>
2020-07-08 12:17:16 +0700

    Fix gwass implementation

--- grayworld-assumption/graywld_ass.py
+++ grayworld-assumption/graywld_ass.py
@@ -1,47 +1,19 @@
-from cv2 import (COLOR_BGR2LAB, COLOR_LAB2BGR, cvtColor,
-                 imread, imshow, merge, split, waitKey)
-
-
-def avg_a_b(lab_img, w, h):
-    sum_a = 0
-    sum_b = 0
-    l, a, b = split(lab_img)
-    # first find average color in CIE Lab space
-    for y in range(h - 1):
-        for x in range(w - 1):
-            sum_a, sum_b = sum_a + a[x, y], sum_b + b[x, y]
-
-    avg_a = sum_a / (w * h)
-    avg_b = sum_b / (w * h)
-    return avg_a, avg_b
-
-
-def shift_a_b(lab_img, a_shift, b_shift, w, h):
-    l, a, b = split(lab_img)
-
-    for y in range(h):
-        for x in range(w):
-            # scale the chroma distance shifted according to amount of
-            # luminance. The 1.1 overshoot is because we cannot be sure
-            # to have gotten the data in the first place.
-            a_delta = a_shift * (l[x, y] / 100) * 1.1
-            b_delta = b_shift * (l[x, y] / 100) * 1.1
-            a[x, y], b[x, y] = a[x, y] + a_delta, b[x, y] + b_delta
-    gwa = merge((l, a, b))
-    return cvtColor(gwa, COLOR_LAB2BGR)
+from cv2 import imread, imshow, waitKey, cvtColor, COLOR_BGR2LAB, COLOR_LAB2BGR
+from numpy import average
 
 
 def grayworld_assumption(img):
-    lab_img = cvtColor(img, COLOR_BGR2LAB)
-    w, h = lab_img.shape[0], lab_img.shape[1]
-    avg_a, avg_b = avg_a_b(lab_img, w, h)
-    return shift_a_b(lab_img, -avg_a, -avg_b, w, h)
+    result = cvtColor(img, COLOR_BGR2LAB)
+    avg_a = average(result[:, :, 1])
+    avg_b = average(result[:, :, 2])
+    result[:, :, 1] = result[:, :, 1] - ((avg_a - 128) * (result[:, :, 0] / 255.0) * 1.1)
+    result[:, :, 2] = result[:, :, 2] - ((avg_b - 128) * (result[:, :, 0] / 255.0) * 1.1)
+    result = cvtColor(result, COLOR_LAB2BGR)
+    return result
 
 
 img = imread('before_img.png')
 imshow('Origin', img)
-waitKey()
-
 res = grayworld_assumption(img)
 imshow('Grayworld Assumption', res)
 waitKey()
